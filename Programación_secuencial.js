c5a173e6edd589e5b6417f65415fd7373f6312b6
kzakiz <juanesteban.bta@gmail.com>
2026-02-25 16:01:16 -0500

    Primera prueba

---
+++ Programación_secuencial.js
@@ -0,0 +1,20 @@
+let nombre = "Juan";
+let edad = 17;
+let apellido = "Villarreal";
+let num = 18;
+
+function sumar(a, b) {
+    return a + b;
+}
+
+function resta(a, b) {
+    let operación = a - b;
+    return operación;
+}
+
+console.log("El resultado de la suma es: " + sumar(num, edad));
+let Resultadoresta = resta(num, edad);
+console.log("El resultado de la resta es: " + Resultadoresta);
+
+console.log("Nombre completo: " + nombre + " " + apellido);
+console.log ("Edad: " + edad);
